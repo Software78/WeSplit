9143906976bd45cc0680709501eb4f9c1dd42483
Owen <technicatalyst78@gmail.com>
2024-01-03 12:49:31 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adedola Owen Abaru on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
